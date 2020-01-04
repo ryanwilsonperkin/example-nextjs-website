c92b564dece7f7e93a3f7b9e40fef56bb5dd2f76
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-04 18:56:06 -0500

    Update Nav
    Only Home and Posts as the links

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import Link from "next/link";
 
-const links = [
-  { href: "https://zeit.co/now", label: "ZEIT" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" }
-];
-
 const Nav = () => (
-  <nav>
+  <nav id="navbar">
     <ul>
       <li>
         <Link href="/">
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ href, label }) => (
-        <li key={`nav-link-${href}-${label}`}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
+      <li>
+        <Link href="/posts">
+          <a>Posts</a>
+        </Link>
+      </li>
     </ul>
 
     <style jsx>{`
