b5f1b7d76bf638ad6a23b172010852095f501a5d
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-04 19:04:05 -0500

    Add posts page

---
+++ pages/posts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../components/head";
+import Nav from "../components/nav";
+
+const Home = () => {
+  return (
+    <>
+      <Head title="Posts" />
+      <Nav />
+    </>
+  );
+};
+
+export default Home;
