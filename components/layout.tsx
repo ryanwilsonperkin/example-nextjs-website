0077da278c34d5a127087e951e6c65cf3ccb8e7a
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-04 19:21:13 -0500

    Add layout

---
+++ components/layout.tsx
@@ -0,0 +1,216 @@
+import React from "react";
+import NextHead from "next/head";
+
+const Layout: React.FC = props => (
+  <>
+    <NextHead>
+      <link
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,700italic|Merriweather:300,300italic,700,700italic"
+        rel="stylesheet"
+        type="text/css"
+      />
+    </NextHead>
+    {props.children}
+    <style jsx global>
+      {`
+        body {
+          color: #444;
+          font-family: "Merriweather", serif;
+          font-weight: 300;
+          font-size: 16px;
+          letter-spacing: 0.02em;
+          line-height: 1.618em;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: "Source Sans Pro", sans-serif;
+          color: #333;
+          margin-bottom: 0.25em;
+        }
+
+        h1 {
+          font-size: 2em;
+        }
+
+        h2 {
+          font-size: 1.625em;
+        }
+
+        h3 {
+          font-size: 1.375em;
+        }
+
+        h4 {
+          font-size: 1.125em;
+        }
+
+        blockquote {
+          margin-left: 0em;
+          border-left: 0.25em solid;
+          padding-left: 2em;
+          font-style: italic;
+          font-size: 1em;
+        }
+
+        a {
+          font-weight: bold;
+          text-decoration: none;
+          color: #428bca;
+        }
+
+        a:visited {
+          color: #428bca;
+        }
+
+        a:hover {
+          color: #428bca;
+        }
+
+        img {
+          max-width: 100% !important;
+          height: auto;
+          display: block;
+        }
+
+        hr {
+          border: 0;
+          height: 0;
+          border-top: 1px solid rgba(0, 0, 0, 0.25);
+        }
+
+        section {
+          max-width: 700px;
+          margin: 0 auto;
+        }
+
+        .title {
+          font-size: 2.5em;
+        }
+
+        .post {
+          margin: 0 auto;
+          padding: 10px;
+        }
+
+        .post p {
+          margin-top: 0;
+          margin-bottom: 2em;
+        }
+
+        .post p:last-of-type {
+          margin-bottom: 0.5em;
+        }
+
+        .post img {
+          margin: 0 auto;
+        }
+
+        .date {
+          color: darkgrey;
+        }
+
+        #title {
+          text-align: center;
+          text-transform: uppercase;
+        }
+
+        #title h1 {
+          margin-bottom: 0;
+          font-size: 6em;
+          line-height: 1em;
+        }
+
+        #title h2 {
+          margin-top: -0.5em;
+          font-size: 1.8em;
+          line-height: 1.25em;
+        }
+
+        #introduction {
+          text-align: center;
+          font-weight: light;
+          max-width: 600px;
+          margin: 2em auto;
+        }
+
+        #posts ul {
+          list-style-type: none;
+          padding: 0;
+        }
+
+        /* Large breakpoint - Desktop */
+        @media only screen and (min-width: 769px) {
+          .title {
+            line-height: 1.05em;
+          }
+          h1 {
+            line-height: 1.05em;
+          }
+          h2 {
+            line-height: 1.25em;
+          }
+          h3 {
+            line-height: 1.25em;
+          }
+          h4 {
+            line-height: 1.22em;
+          }
+          blockquote {
+            line-height: 1.4em;
+          }
+        }
+
+        /* Medium breakpoint - Tablet */
+        @media only screen and (min-width: 481px) and (max-width: 768px) {
+          .title {
+            line-height: 1.125em;
+          }
+          h1 {
+            line-height: 1.125em;
+          }
+          h2 {
+            line-height: 1.25em;
+          }
+          h3 {
+            line-height: 1.25em;
+          }
+          h4 {
+            line-height: 1.2222em;
+          }
+          blockquote {
+            line-height: 1.4em;
+          }
+        }
+
+        /* Small breakpoint - Phone */
+        @media only screen and (max-width: 480px) {
+          .title {
+            line-height: 1.25em;
+          }
+          h1 {
+            line-height: 1.25em;
+          }
+          h2 {
+            line-height: 1.1538em;
+          }
+          h3 {
+            line-height: 1.1363em;
+          }
+          h4 {
+            line-height: 1.1111em;
+          }
+          blockquote {
+            line-height: 1.4em;
+          }
+        }
+      `}
+    </style>
+  </>
+);
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,119 +2,38 @@ import React from "react";
 import Link from "next/link";
 import Head from "../components/head";
 import Nav from "../components/nav";
+import Layout from "../components/layout";
 
 const Home = () => {
   return (
-    <div>
+    <Layout>
       <Head title="Home" />
       <Nav />
 
-      <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
-        <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{" "}
-          <code>pages/api/date.js</code> files, then save to reload.
+      <section id="title">
+        <h1>Ryan</h1>
+        <h2>Wilson-Perkin</h2>
+      </section>
+      <section id="introduction">
+        <p>
+          Hey! My name's Ryan, I'm a full-stack developer living in beautiful
+          Toronto, Ontario.
         </p>
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
-        </div>
-      </div>
-
-      <style jsx>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .date {
-          height: 24px;
-          max-width: calc(100% - 32px)
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 16px;
-        }
-        .date p {
-          text-align: center;
-        }
-        .date span {
-          width: 176px;
-          text-align: center;
-        }
-        @keyframes Loading {
-          0%{background-position:0% 50%}
-          50%{background-position:100% 50%}
-          100%{background-position:0% 50%}
-        }
-        .date .loading {
-          max-width: 100%;
-          height: 24px;
-          border-radius: 4px;
-          display: inline-block;
-          background: linear-gradient(270deg, #D1D1D1, #EAEAEA);
-          background-size: 200% 200%;
-          animation: Loading 2s ease infinite;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
-    </div>
+        <p>
+          I graduated from the University of Guelph where I helped run the{" "}
+          <a href="http://socis.ca">
+            Society of Computing and Information Science
+          </a>
+          .
+        </p>
+        <p>
+          You can reach me by{" "}
+          <a href="mailto:ryanwilsonperkin@gmail.com">email</a>, follow me on{" "}
+          <a href="https://twitter.com/rwilsonperkin">Twitter</a>, or check out
+          my code on <a href="https://github.com/ryanwilsonperkin">GitHub</a>.
+        </p>
+      </section>
+    </Layout>
   );
 };
 
