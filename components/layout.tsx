a35f325a6b5a73bbea62dde563e04a883cd04e95
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-04 19:36:59 -0500

    Add google analytics script

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import NextHead from "next/head";
 
+const Fonts: React.FC = () => (
+  <link
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,700italic|Merriweather:300,300italic,700,700italic"
+    rel="stylesheet"
+    type="text/css"
+  />
+);
+
+const GoogleAnalyticsScript: React.FC = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-44964499-2', 'auto');
+        ga('send', 'pageview');
+      `
+    }}
+  />
+);
+
 const Layout: React.FC = props => (
   <>
     <NextHead>
-      <link
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,700italic|Merriweather:300,300italic,700,700italic"
-        rel="stylesheet"
-        type="text/css"
-      />
+      <Fonts />
+      <GoogleAnalyticsScript />
     </NextHead>
     {props.children}
     <style jsx global>
