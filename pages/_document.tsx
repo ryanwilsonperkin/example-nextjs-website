98aac2949f6b2426a9e5952c9ab9de0695f1a9d0
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2020-01-06 20:06:48 -0500

    Set lang=en on html tag by overriding document

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+/* 
+  Extend the default document definition.
+  See more at https://github.com/zeit/next.js#custom-document
+*/
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
